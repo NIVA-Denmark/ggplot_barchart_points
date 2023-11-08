5146d61afa9e4d8f8fec89da5ad5c11d4a617704
Ciarán Murray <cjm@niva-dk.dk>
2023-11-08 17:37:45 +0100

    first commit

--- plot.R
+++ plot.R
@@ -1,22 +1,21 @@
-ggplot - plot bars as points
-================
+library(renv)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(ggimage)
+library(terra)
+library(tidyterra)
 
-A plain R script with the code is here: [plot.R](./plot.R)
+# -------- test data -----------------------
 
-# Test data
-
-Create test dataframe for stations
-
-``` r
+# create test data frame for stations
 id <- c(1,2,3,4) # station id
 lon <- c( 6.1,  8.7,  9.4, 11.9)
 lat <- c(55.9, 55.2, 57.1, 54.9)
 df_stns <- data.frame(id=id, lon=lon, lat=lat)
-```
 
-Create test dataframe for bar chart data
+# create test data.frame for bar charts
 
-``` r
 set.seed(99)
 
 Species <- c("a","b","c","d","e")
@@ -25,38 +24,26 @@ Category <- c("x","y","z")
 df_bar_data <- merge(data.frame(Species=factor(Species,levels=Species)),
                  data.frame(Category=factor(Category, levels=Category)),
                  all=T)
-```
 
-we include station id with the test data to give a postion to each bar
-chart
-
-``` r
+# we include station id to match the positions
 df_bar_data <- merge(data.frame(id=id),
                      df_bar_data,
                  all=T)
-```
 
-add random values for our y-variable “n”
-
-``` r
+# add random values for our y-variable "n"
 df_bar_data <- df_bar_data %>%
   mutate(n=rnorm(nrow(df_bar_data), mean=10, sd=4)) %>%
   mutate(n=ifelse(n<0,0,n))
-```
 
-normalise values to as fractions of the total within each
-category/station combination
 
-``` r
+# calculate as fractions of the total within each category/stn
 df_bar_data <- df_bar_data %>%
   group_by(id, Category) %>%
   mutate(n=n/sum(n,na.rm=T)) %>%
   ungroup()
-```
 
-# Function to make individual bar charts
 
-``` r
+# function to make individual bar charts
 bar_chart <- function(selected_id=NA,
                       df,
                       xvar="Category",
@@ -83,71 +70,44 @@ bar_chart <- function(selected_id=NA,
   ggsave(p, filename=imgfile, width=size_x, height=size_y, units="px", dpi=600)  
   return(imgfile)
 }
-```
 
-# Make the bar charts
-
-Make the bar chart for each station and save them locally
-
-``` r
+# do the bar charts for each station
 id_list <- unique(df_bar_data$id)
 plot_list <-  purrr::map(id_list, bar_chart, df=df_bar_data) %>% 
   unlist()
-```
 
-Add the list of image files to the station data frame
-
-``` r
+# add the image information to the stations dataframe
 df_stns$bar_img <- plot_list
-```
 
-# Make the plot
 
-Get a shape file for the Danish EEZ
-
-``` r
+# get a shape file for the Danish EEZ
 eez <- terra::vect("shp/EEZ_polygon.shp")
 terra::crs(eez) <-  terra::crs("EPSG:4326") # add the CRS
-```
 
-### Simple map with EEZ and points
-
-``` r
+# ---- simple plot with EEZ and points  ----
 ggplot() +
   geom_spatvector(data=eez) +
   geom_point(data=df_stns,aes(x=lon,y=lat), colour="#FF0000") +
   geom_text(data=df_stns,aes(x=lon,y=lat,label=id),
             hjust=1, vjust=1)
-```
 
-![](README_files/figure-gfm/plot%20stations-1.png)<!-- -->
 
-### Map with bar charts
-
-The bars are introduced to the ggplot as images
-
-We need to find another way to add the legend for the bars to the map.
-
-We can do this by plotting a layer which uses the same fill variable as
-the bar charts (Species). We will use a geom_rect layer - this plots
-rectangles with fill according to species. Because we use the same
-x-values for xmin and xmax and the same y-values for ymin and ymax, then
-the rectangles will have zero height and width. Therefore they should
-not be visible but they *will* trigger the creation of a legend with
-species colours.
-
-``` r
+# ---- make a dataframe to use as legend for the map  ----
 df_legend <- df_bar_data %>%
   distinct(Species) %>%
   mutate(n=1)
 
 df_legend <- df_stns %>% 
   merge(df_legend, all=T)
-```
 
-And here is the plot:
 
-``` r
+#  ---- plot with EEZ and bars  ----
+
+# the geom_rect plots rectangles with fill according to species
+# because we use the same x-values for xmin and xmax and the samew
+# y-values for ymin and ymax, they should not be visible
+# but they *will* trigger a legend with species colours
+
 ggplot() +
   geom_rect(data=df_legend,
             aes(xmin=lon, xmax=lon, ymin=lat, ymax=lat, fill=Species)) +
@@ -157,9 +117,4 @@ ggplot() +
             hjust=1, vjust=1) +
   geom_image(data=df_stns,aes(x=lon,y=lat,image=bar_img),
              nudge_x=0.5, nudge_y=0.5, size=0.1) 
-```
 
-![](README_files/figure-gfm/final%20plot-1.png)<!-- -->
-
-You will need to mess around with the themes and formatting in the bar
-chart plot function and with the size and positioning of the bars.
